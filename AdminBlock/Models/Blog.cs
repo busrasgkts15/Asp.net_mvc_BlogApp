0988b90784251c759fe175d7984b47ab1c040955
Büşra Göktaş <busra1gkts@gmail.com>
2024-06-27 13:49:37 +0300

    Adding dotnet package

---
+++ AdminBlock/Models/Blog.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel;
+
+namespace AdminBlock.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Content { get; set; }
+        public string Imagepath { get; set; }
+        public bool IsPublish { get; set; }
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+
+    }
+}
---
+++ AdminBlock/Models/BlogContext.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminBlock.Models
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Blog> Blog { get; set; }
+
+
+    }
+}
